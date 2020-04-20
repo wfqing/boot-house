967796bd9c2e638fb0474a1d23a7ab9b6bc4ae9d
wfqing‘git config --global user.email  1360639558@qq.comcat .git/configgit config --global user.name wfqing‘ <1360639558@qq.com>
2020-04-20 14:54:02 +0800

    dev分支开发User.java

---
+++ src/main/java/com/etoak/bean/User.java
@@ -0,0 +1,16 @@
+package com.etoak.bean;
+
+import lombok.Data;
+
+/**
+ * Created by wfqing on 2020/4/20.
+ */
+@Data
+public class User {
+    private Integer id;
+    private String name;
+    private String password;
+    private String gender;
+    private Integer age;
+    private String birthday;
+}
