cdc5594dfa9a4e4e6737ccd3f69ce256116dd5ee
wfqing‘git config --global user.email  1360639558@qq.comcat .git/configgit config --global user.name wfqing‘ <1360639558@qq.com>
2020-04-20 11:36:57 +0800

    主方法和yml文件书写

--- src/main/java/com/etoak/HouseApp.java
+++ src/main/java/com/etoak/HouseApp.java
@@ -1,7 +1,14 @@
 package com.etoak;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * Created by wfqing on 2020/4/20.
  */
+@SpringBootApplication
 public class HouseApp {
+    public static void main(String[] agrs) {
+        SpringApplication.run(HouseApp.class, agrs);
+    }
 }
