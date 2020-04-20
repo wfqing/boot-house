e45e352e0594a750a58efdfb57a5b672ce69a26c
wfqing‘git config --global user.email  1360639558@qq.comcat .git/configgit config --global user.name wfqing‘ <1360639558@qq.com>
2020-04-20 17:49:53 +0800

    配置swagger

--- src/main/java/com/etoak/controller/AreaController.java
+++ src/main/java/com/etoak/controller/AreaController.java
@@ -2,6 +2,9 @@ package com.etoak.controller;
 
 import com.etoak.bean.Area;
 import com.etoak.service.AreaService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -15,11 +18,14 @@ import java.util.List;
 @RestController
 @Slf4j
 @RequestMapping("/area")
+@Api(tags = "地区查询服务")
 public class AreaController {
     @Autowired
     AreaService areaService;
 
     @GetMapping("/queryById")
+    @ApiOperation(value = "根据父id查询地区列表", notes = "根据父id查询地区列表")
+    @ApiImplicitParam(value = "父id", name = "pid", required = false, defaultValue = "0", dataType = "int")
     public List<Area> queryById(@RequestParam(required = false, defaultValue = "0") int pid){
         return areaService.queryByPid(pid);
     }
