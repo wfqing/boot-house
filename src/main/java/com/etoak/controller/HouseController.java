5f69a11bfd377f72e3abcdaea6c6c1049c451338
wfqing‘git config --global user.email  1360639558@qq.comcat .git/configgit config --global user.name wfqing‘ <1360639558@qq.com>
2020-04-21 17:38:33 +0800

    添加页面的创建

---
+++ src/main/java/com/etoak/controller/HouseController.java
@@ -0,0 +1,17 @@
+package com.etoak.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by wfqing on 2020/4/21.
+ */
+@Controller
+@RequestMapping("/house")
+public class HouseController {
+
+    @RequestMapping("/toAdd")
+    public String toAdd(){
+        return "/house/add";
+    }
+}
