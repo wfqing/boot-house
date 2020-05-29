89bc29bc4f452ad31581da8f940a691face11f10
wfqing <w1360639558q>
2020-05-29 16:35:35 +0800

    试试idea6

--- src/main/java/com/etoak/HouseApp.java
+++ src/main/java/com/etoak/HouseApp.java
@@ -13,7 +13,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 //eclipse
 //idea
-/////
+/////9898
 //456
 public class HouseApp {
     public static void main(String[] agrs) {
