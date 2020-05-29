763313eb1d58c6daf9119a3bc1ecb77b12f31ff3
wfqing <w1360639558q>
2020-05-29 17:42:05 +0800

    3333333

--- src/main/java/com/etoak/HouseApp.java
+++ src/main/java/com/etoak/HouseApp.java
@@ -10,7 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 @MapperScan(basePackages = "com.etoak.mapper")
-
+//
 public class HouseApp {
     public static void main(String[] agrs) {
         SpringApplication.run(HouseApp.class, agrs);
